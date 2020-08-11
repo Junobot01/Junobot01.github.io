927638ff52c1ef7899c122819d76321ffacab558
Junobot01 <69430218+Junobot01@users.noreply.github.com>
2020-08-12 03:48:04 +0900

    gameStart added
    'Click to start the game' implemented

--- Flappy_Bird/sketch.js
+++ Flappy_Bird/sketch.js
@@ -24,15 +24,20 @@ var canvasHeight = 600;
 var frame = 1;
 
 // Image vars
+var background_src;
 var background_image;
 var bird_whole_image;
 var bird_image;
 var bird_image_list = [];
 var bird_image_count = 3;
 
+// Game vars
+var gameStart = false;
+
 function preload() {
 
-	background_image = loadImage("assets/background.png");
+	background_image = createImage(canvasWidth, canvasHeight);
+	background_src = loadImage("assets/background.png");
 	bird_whole_image = loadImage("assets/bird.png")
 
 }
@@ -42,60 +47,43 @@ function setup() {
 
 	canvas = createCanvas(canvasWidth, canvasHeight);
 	canvas.center('horizontal');
-
-	background(0); // ! TEMPORARY CODE FOR SHOWING CANVAS ! ! TEMPORARY CODE FOR SHOWING CANVAS ! ! TEMPORARY CODE FOR SHOWING CANVAS !
 	
+	background_src.resize(canvas.width, canvas.height*(1-groundHeight));
+	background_image.copy(background_src, 0, 0, background_src.width, background_src.height, 0, 0, background_src.width, background_src.height);
 	
-	background_image.resize(canvas.width, canvas.height*(1-groundHeight));
 
 	player = new Bird();
 
 // Split bird image
-	for (var i=0; i<bird_image_count; i++) {
-		var img = bird_whole_image.get(i*bird_whole_image.width/bird_image_count, 0, bird_whole_image.width/bird_image_count, bird_whole_image.height);
-		img.resize(birdSize*img.width/img.height, birdSize);
-		bird_image_list.push(img);
-	}
-	for (var i=bird_image_count-2; i>0; i--) {
-		var img = bird_whole_image.get(i*bird_whole_image.width/bird_image_count, 0, bird_whole_image.width/bird_image_count, bird_whole_image.height);
+	for (var i=0; i<2*bird_image_count-3; i++) {
+		var j = bird_image_count-1-Math.abs(i-bird_image_count+1)
+		var img = bird_whole_image.get(j*bird_whole_image.width/bird_image_count, 0, bird_whole_image.width/bird_image_count, bird_whole_image.height);
 		img.resize(birdSize*img.width/img.height, birdSize);
 		bird_image_list.push(img);
 	}
 	bird_image = bird_image_list[frame];
 
-
-/*********************************************************************************************************
- ***************************** * TEMPORARY CODE FOR CHECKING IMAGES **************************************
- *********************************************************************************************************
-	for (var i=0; i<bird_image_count; i++) {
-		var img = bird_image_list[i]
-		image(img,(canvas.width-img.width)/2.0, (2*i+1)*canvas.height/bird_image_count/2-img.height/2);
-	}
- ********************************************************************************************************/
 }
 
 function draw() {
 
-	image(background_image, 0, 0);
-
-	if (humanPlaying) {
-		player.update();
-	}
-
-	player.vel.y += gravity;
-
+	background(background_image);
 	bird_image = bird_image_list[Math.floor(frame/flapSpeed)%bird_image_list.length];
 	image(bird_image, player.pos.x-bird_image.width/2.0, player.pos.y-bird_image.height/2.0);
 
+	if (gameStart) {
+		player.update();	
+		player.vel.y += gravity;		
+	}
+
 	frame++;
-	stroke(0);
-	line(0,canvas.height*0.98,canvas.width,canvas.height*0.95);
 
 }
 
 function mouseClicked() {
 	
 	player.vel.y = jump;
+	gameStart = true;
 
 }
 
